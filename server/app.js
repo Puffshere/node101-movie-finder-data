05b17d81c05a32ca83aa7ebecdf59f21f6d3923d
Shawn Taylor <shawncurtistaylor@gmail.com>
2019-11-21 23:47:57 -0800

    initial commit

--- server/app.js
+++ server/app.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const axios = require('axios');
+const logger = require('morgan');
 
-
+const PORT = 3000
 const app = express();
+app.use(logger('combined'))
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+let cache = {};
+
+app.get('/', (req, res) => {
+  let reqURL = req.url;
+  let siteURL = 'http://www.omdbapi.com' + reqURL + '&apikey=bebfe443';
+
+  if (!Object.keys(cache).includes(reqURL)) {
+    axios.get(siteURL)
+      .then((response) => {
+        cache[reqURL] = response.data
+        res.send(response.data)
+      })
+      .catch((error) => {
+        res.send(error)
+      })
+  } else {
+    res.send(cache[reqURL])
+  }
+})
 
 module.exports = app;
